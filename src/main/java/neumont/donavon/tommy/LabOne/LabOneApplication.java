c5f7371af7f44a4f1abcb181850a771dc637acfa
yhung-mea7 <60412916+yhung-mea7@users.noreply.github.com>
2021-01-14 18:22:50 -0700

    init

---
+++ src/main/java/neumont/donavon/tommy/LabOne/LabOneApplication.java
@@ -0,0 +1,13 @@
+package neumont.donavon.tommy.LabOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neumont/donavon/tommy/LabOne/LabOneApplicationTests.java
@@ -0,0 +1,13 @@
+package neumont.donavon.tommy.LabOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
