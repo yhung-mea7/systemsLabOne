a090d14c616768fb2e8c65ad3b6fa6dff3034d8f
yhung-mea7 <60412916+yhung-mea7@users.noreply.github.com>
2021-02-05 12:03:11 -0700

    lab two done

--- src/main/java/neumont/donavon/tommy/LabOne/LabOneApplication.java
+++ src/main/java/neumont/donavon/tommy/LabOne/LabOneApplication.java
@@ -3,10 +3,31 @@ package neumont.donavon.tommy.LabOne;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+import javax.servlet.DispatcherType;
+import java.util.List;
+
+
 @SpringBootApplication
 public class LabOneApplication {
 
+	@Bean
+	FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter()
+	{
+		ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+		FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
+		registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+		registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		registration.setUrlPatterns(List.of("/absoluteURLWithFilter"));
+		return registration;
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(LabOneApplication.class, args);
 	}
 
