f1d9470a1166533bc68c28db1b637218592a73d5
yhung-mea7 <60412916+yhung-mea7@users.noreply.github.com>
2021-02-05 15:06:05 -0700

    Update DockerRestController.java

--- src/main/java/neumont/donavon/tommy/LabOne/controllers/DockerRestController.java
+++ src/main/java/neumont/donavon/tommy/LabOne/controllers/DockerRestController.java
@@ -16,12 +16,14 @@ import java.util.Map;
 @RequestMapping
 public class DockerRestController {
 
+    private static int count = 0;
 
     @GetMapping
     public HttpEntity<Map<String, String>> roundRobin() throws UnknownHostException {
         String s = InetAddress.getLocalHost().getHostName();
         Map<String, String> response = new HashMap<>();
         response.put("Message", "Heya I've been hit at " + s);
+        response.put("count", String.format("Amount of requests: %s", ++count));
         System.out.println("I've been hit!" + s);
         return new ResponseEntity<>(response, HttpStatus.OK);
 
